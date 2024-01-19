5e591acad54fe2f6f741937a56626b62fd39f747
Andersonmelodamasceno <dinho.live01@gmail.com>
2024-01-19 00:29:37 -0300

    Metodo Get Funcionando

---
+++ Data/Veiculos.cs
@@ -0,0 +1,12 @@
+﻿namespace Veiculos.Api.Data
+{
+            public class Veiculos
+        {
+            public int Id { get; set; }
+            public string Placa { get; set; }
+            public string Cor { get; set; }
+            public int Ano { get; set; }
+        }
+ }
+
+
